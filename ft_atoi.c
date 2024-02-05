e2f61cf6fb0ad8bbc38d938e709bcd8602d569e8
Caroline Oliveira <104214541+Dizziolica@users.noreply.github.com>
2024-02-05 23:32:54 +0000

    Add files via upload

---
+++ ft_atoi.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdio.h>
+int ft_atoi( char *str){
+   int i = 0;
+   while (palavra[i] = '\0'){
+	  printf("%d", palavra[i]);
+	  i++;
+      
+   
+   }    
+
+
+}
+
+int main(){
+
+        char palavra[] = "white space";
+	int numero = ft_atoi(&palavra);
+	printf("%d", numero);
+}
